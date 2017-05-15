deb2f843f4a62e22f4b152d175271515b04cbda9
wookienz <y2r42ehg>
2017-05-15 19:31:50 +0800

    add config file.

---
+++ config.py
@@ -0,0 +1,16 @@
+secret = "aeffI"
+baseurl = "https://api.vhack.cc/v/7/"
+user = "username"
+password = "password"
+botNetServers = 3
+attackchance = 90
+logfile = 'vhack.log'
+logging_level = 'INFO'
+
+# Enter Max Antivir to attack in normal mode
+maxanti_normal = 500
+# Finish all task by netcoins
+Use_netcoins = True
+# Enter Amount of Attacks normal
+attacks_normal = 3
+winchance = 90
